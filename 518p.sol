1d98e21dffd3f1eecf82b7dacdb066f5568d0bc0
WhiteNetSource <9oodvibe5only@gmail.com>
2023-05-23 00:10:21 +0900

    523

--- 518p.sol
+++ 518p.sol
@@ -1,19 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Q20 {
-	uint[] numbers;
-	function pushNumbers() public {
-		for(uint i=0; i<15; i++) {
-			numbers.push(i+1);
-		}
-		
-		for(uint i=3;i<=15;i+=3) {
-			delete numbers[i-1];
-		}
-	}
+contract Time{
+ 
+// 숫자를 시분초로 변환하세요.
+// 예) 100 -> 1분 40초, 600 
+// -> 10분, 1000 -> 16분 40초,
+//  5250 -> 1시간 27분 30초
 
-      function getArr() public view returns(uint[] memory) {
-        return numbers;
+    struct T {
+        uint hour;  
+        uint minute;
+        uint second;
     }
+
+    
+   T times[3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};// <- 어케 넣지
+
+  function getTime(uint time) public pure returns(uint) {
+        
+        uint hour = time / 3600;
+        uint minute = time % 3600 / 60;
+        uint second = time % 60 % 60;
+
+        if (time < 60) {
+            return second;
+        } else if (time < 3600) {
+            return minute;
+        } else {
+            return hour;
+        }
+    }
+
 }
