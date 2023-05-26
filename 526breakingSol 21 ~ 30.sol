19e1c88fb7c0952a6a382688ee8c04f2ca9f48a9
WhiteNetSource <9oodvibe5only@gmail.com>
2023-05-27 02:02:08 +0900

    527

--- 526breakingSol 21 ~ 30.sol
+++ 526breakingSol 21 ~ 30.sol
@@ -21,12 +21,31 @@ contract Q21 {
 //     예) 2,5 input → 5-2=3(output)
 
 contract Q22 {
-
-    function sub( uint _n , uint _n1 ) public pure returns( uint ) {
-        if( _n > _n1 ) {
-            return _n - _n1 ;
+    function sub( uint _a , uint _b ) public pure returns( uint ) {
+        if( _a > _b ) {
+            return _a - _b ; 
         } else {
-            return _n1 - _n;
+            return _b - _a ;
         }
     }
 }
+
+// 3의 배수라면 “A”를, 
+// 나머지가 1이 있다면 “B”를,
+//  나머지가 2가 있다면 “C”를 반환하는 함수를 구현하세요.
+
+contract Q23 {
+    function returnStrings( uint _n ) public pure returns( string memory ) {
+        if( 3 % _n == 0 ) {
+            return "A" ;
+        } else if( 3 % _n == 1 ) {
+            return "B" ;
+        } else {
+            return "C" ;
+        }
+    }
+}
+
+// string을 input으로 받는 함수를 구현하세요.
+//  “Alice”가 들어왔을 때에만 true를 반환하세요.
+
