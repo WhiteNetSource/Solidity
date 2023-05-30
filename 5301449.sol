392323943293f7ecb36e0ae55e5d85fe5bd8eaad
WhiteNetSource <9oodvibe5only@gmail.com>
2023-05-30 14:50:18 +0900

    530

---
+++ 5301449.sol
@@ -0,0 +1,358 @@
+황재윤
+황재윤#7069
+
+하유민 — 오늘 오후 2:29
+쎈수학 갑시다
+서재민 — 오늘 오후 2:29
+ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+하유민 — 오늘 오후 2:29
+ㅠㅜㅠ
+조성윤 — 오늘 오후 2:29
+ㅋㅋㅋㅋㅋ
+하유민 — 오늘 오후 2:29
+성윤님 팀원이라구요
+봐주세요
+ㅠㅜㅠㅜ
+쎈수학 그룹
+서재민 — 오늘 오후 2:29
+고등학생 >>>>>>>>>블체스 ㅠㅠ
+블체스 고등학새보다.. 못한...다........
+김시온 — 오늘 오후 2:30
+ㅋㅋ
+하유민 — 오늘 오후 2:30
+ㅠㅜㅠㅠㅜ
+[매니저] 장이화 — 오늘 오후 2:30
+블체스 살려.. :meow_bandage:
+하유민 — 오늘 오후 2:30
+과외를 구해야하나
+ㅠㅜㅠㅜㅠㅜㅠ
+조성윤 — 오늘 오후 2:30
+네네 !!
+ㅋㅋㅋㅋㅋㅋ
+2자리요
+255
+99
+조하빈 — 오늘 오후 2:31
+99
+조성윤 — 오늘 오후 2:32
+휴~
+하유민 — 오늘 오후 2:32
+배신자 수학잘하자나요
+조성윤 — 오늘 오후 2:32
+암기 과목이더라구요 ㅠ
+김시온 — 오늘 오후 2:32
+오~ 조성윤~
+조성윤 — 오늘 오후 2:33
+넵
+1바이트
+조하빈 — 오늘 오후 2:34
+바이트1이라
+김시온 — 오늘 오후 2:34
+용량이 달라서
+조하빈 — 오늘 오후 2:34
+bytes2
+[강사] 박민서 — 오늘 오후 2:35
+contract BytesCompare {
+    bytes1 a = 0x12;
+    bytes1 b = 0x23;
+
+    function compare() public view returns(bool) {
+        return b>a;
+    }
+
+    function compare2(bytes1 _a, bytes1 _b) public pure returns(bool) {
+        return _b>_a;
+    }
+
+    function bytes1ToUint(bytes1 _a) public pure returns(uint8) {
+        return uint8(_a);
+    }
+
+    function uintToBytes1(uint8 _a) public pure returns(bytes1) {
+        return bytes1(_a);
+    }
+}
+조성윤 — 오늘 오후 2:35
+5
+김시온 — 오늘 오후 2:35
+0x35
+조성윤 — 오늘 오후 2:36
+0x35 =5?
+권세명 — 오늘 오후 2:36
+잘못했다해요
+빨리
+조성윤 — 오늘 오후 2:36
+죄송합니다
+김강현 — 오늘 오후 2:36
+a0? 
+조성윤 — 오늘 오후 2:36
+0x35가 hex 값 거쳐서 5가 나오는줄 알았어요
+하유민 — 오늘 오후 2:36
+울지말고 말해요 ㅠ
+네
+조성윤 — 오늘 오후 2:37
+마이크좀 켜ㅑ도되나요?
+김시온 — 오늘 오후 2:37
+ㅋㅋ
+조성윤 — 오늘 오후 2:37
+네네
+김시온 — 오늘 오후 2:37
+싸워라! 싸워라!
+하유민 — 오늘 오후 2:37
+마이크 기각
+ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+조성윤 — 오늘 오후 2:37
+손이 아파서 ㅠ
+권세명 — 오늘 오후 2:37
+ㅋㅋㅋㅋㅋ
+김백기 — 오늘 오후 2:37
+ㅋㅋㅋㅋㅋㅋㅋㅋ
+김시온 — 오늘 오후 2:38
+ㅋㅋ
+하유민 — 오늘 오후 2:38
+ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+김백기 — 오늘 오후 2:39
+ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+김영도 — 오늘 오후 2:39
+귀여웠다
+조성윤 — 오늘 오후 2:39
+죄송합니다
+하유민 — 오늘 오후 2:39
+영재구만
+권세명 — 오늘 오후 2:39
+천재였다..
+하유민 — 오늘 오후 2:39
+못알아봐서 미안합니다
+수학과외 부탁드려요
+김시온 — 오늘 오후 2:39
+테이블을 다 외우신게 아닐까요
+하유민 — 오늘 오후 2:39
+네
+64
+신영규 — 오늘 오후 2:40
+64
+황재윤 — 오늘 오후 2:40
+64
+조성윤 — 오늘 오후 2:40
+0x64!
+김백기 — 오늘 오후 2:40
+0x64
+신영규 — 오늘 오후 2:40
+char 어떻게 읽어요?
+전도현 — 오늘 오후 2:40
+캐릭터
+조성윤 — 오늘 오후 2:40
+8비트
+김백기 — 오늘 오후 2:40
+크아아알~
+박동우 — 오늘 오후 2:41
+24비트?
+조하빈 — 오늘 오후 2:41
+24비트
+김강현 — 오늘 오후 2:41
+24?
+조성윤 — 오늘 오후 2:41
+24비트!
+김시온 — 오늘 오후 2:41
+foo
+조성윤 — 오늘 오후 2:41
+jsy?
+ㅋㅋㅋㅋㅋㅋ
+김백기 — 오늘 오후 2:41
+ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+신영규 — 오늘 오후 2:41
+????
+하유민 — 오늘 오후 2:41
+ㅈㅅㅇ
+김영도 — 오늘 오후 2:42
+천재를 지칭
+조성윤 — 오늘 오후 2:42
+넵 이해했습니다
+김시온 — 오늘 오후 2:42
+더하고 바이츠로 감싸요
+하유민 — 오늘 오후 2:43
+넵
+김백기 — 오늘 오후 2:44
+넵
+고종우 — 오늘 오후 2:45
+18
+전도현 — 오늘 오후 2:45
+1b
+고종우 — 오늘 오후 2:45
+죄송합니다
+하유민 — 오늘 오후 2:45
+구몬 추천드려요
+고종우 — 오늘 오후 2:45
+네
+고종우 — 오늘 오후 2:45
+이미 구독..
+하유민 — 오늘 오후 2:45
+도현님?
+권세명 — 오늘 오후 2:45
+ㅋㅋㅋㅋ
+김백기 — 오늘 오후 2:46
+❓
+김영도 — 오늘 오후 2:46
+ㅋㅋㅋ
+하유민 — 오늘 오후 2:46
+예???
+소인수분헤???
+???????????
+예?
+[강사] 박민서 — 오늘 오후 2:48
+// SPDX-License-Identifier: GPL-3.0
+
+/*
+로또 프로그램을 만드려고 합니다. 숫자와 문자는 각각 4개 2개를 뽑습니다. 
+6개가 맞으면 1이더, 5개의 숫자가 순서와 함께 맞으면 0.75이더, 
+4개가 맞으면 0.25이더, 3개가 맞으면 0.1이더 2개 이하는 상금이 없습니다. 
+확장
+message.txt
+6KB
+﻿
+// SPDX-License-Identifier: GPL-3.0
+
+/*
+로또 프로그램을 만드려고 합니다. 숫자와 문자는 각각 4개 2개를 뽑습니다. 
+6개가 맞으면 1이더, 5개의 숫자가 순서와 함께 맞으면 0.75이더, 
+4개가 맞으면 0.25이더, 3개가 맞으면 0.1이더 2개 이하는 상금이 없습니다. 
+
+참가 금액은 0.05이더이다.
+
+기준 숫자 : 1,2,3,4,A,B
+-----------------------------------------------------------------
+기준 숫자 설정 기능 : 5개의 사람이 임의적으로 4개의 숫자와 2개의 문자를 넣음. 
+5명이 넣은 숫자들의 중앙값과 알파벳 순으로 따졌을 때 가장 가운데 문자로 설정
+예) 숫자들의 중앙값 : 1,3,6,8,9 -> 6 // 2,3,4,8,9 -> 4
+예) 문자들의 중앙값 : a,b,e,h,l -> e // a,h,i,q,z -> i
+*/
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Q8 {
+    uint[] numbers = [1,2,3,4];
+    string[] letters = ["a", "b"];
+
+    // 로또 번호 입력, 맞추는기능 - 유저가 로또 번호를 입력하는 기능, 로또 번호를 맞추는 기능
+    function setLotto(uint[] calldata _numbers, string[] calldata _letters) public payable {
+        require(msg.value==0.05 ether);
+        uint fit = isSameNumber(_numbers) + isSameLetter(_letters);
+        getPrize(fit);
+    }
+
+    function getPrize(uint _n) private /*public으로 하면 자선사업가*/ {
+        if(_n==6) {
+            payable(msg.sender).transfer(1 ether);
+        } else if(_n==5) {
+            payable(msg.sender).transfer(0.75 ether);
+        } else if(_n==4) {
+            payable(msg.sender).transfer(0.25 ether);
+        } else if(_n==3) {
+            payable(msg.sender).transfer(0.1 ether);
+        }
+    }
+
+    function isSameNumber(uint[] calldata _myNumber) public view returns(uint) {
+        uint count;
+        for(uint i=0; i<numbers.length; i++) {
+            if(numbers[i]==_myNumber[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    function isSameLetter(string[] calldata _myLetter) public view returns(uint) {
+        uint count;
+        for(uint i=0; i<letters.length; i++) {
+            if(letterCompare(letters[i], _myLetter[i])) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    function letterCompare(string memory _a, string calldata _b) public pure returns(bool) {
+        return keccak256(abi.encodePacked(_a)) == keccak256(abi.encodePacked(_b));
+    }
+}
+
+contract Q8_2 {
+    string[] public letters;
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function setTargetNumber(string[] memory _letters) public {
+        require(owner == msg.sender);
+        letters = _letters;
+    }
+
+    // 로또 번호 입력, 맞추는기능 - 유저가 로또 번호를 입력하는 기능, 로또 번호를 맞추는 기능
+    function setLotto(string[] calldata _letters) public payable {
+        require(msg.value==0.05 ether);
+        uint fit = isSameLetter(_letters);
+        getPrize(fit);
+    }
+
+    function getPrize(uint _n) private /*public으로 하면 자선사업가*/ {
+        if(_n==6) {
+            payable(msg.sender).transfer(1 ether);
+        } else if(_n==5) {
+            payable(msg.sender).transfer(0.75 ether);
+        } else if(_n==4) {
+            payable(msg.sender).transfer(0.25 ether);
+        } else if(_n==3) {
+            payable(msg.sender).transfer(0.1 ether);
+        }
+    }
+
+    function isSameLetter(string[] calldata _myLetter) public view returns(uint) {
+        uint count;
+        for(uint i=0; i<letters.length; i++) {
+            if(letterCompare(letters[i], _myLetter[i])) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    function letterCompare(string memory _a, string calldata _b) public pure returns(bool) {
+        return keccak256(abi.encodePacked(_a)) == keccak256(abi.encodePacked(_b));
+    }
+}
+
+contract Q8_3 {
+    // 로또 번호 설정 기능
+    function setLotto(string[] memory _set1, string[] memory _set2, string[] memory _set3, string[] memory _set4, string[] memory _set5) public view returns(string[] memory) {
+        string[] memory a = new string[](6);
+        for(uint i=0; i<4; i++) {
+            
+        }
+        return a;
+    }
+
+    function bytesAverage(bytes1 a, bytes1 b) public pure returns(bytes1) {
+        return bytes1((uint8(a) + uint8(b))/2);
+    }
+ 
+    function getMedian_Number(bytes1[] memory numbers) public view returns(bytes1){
+        uint _l = numbers.length;
+        for(uint i=0;i<_l-1;i++) {
+            for(uint j=i+1; j<_l ;j++) {
+                if(numbers[i] < numbers[j]) {
+                    (numbers[i], numbers[j]) = (numbers[j], numbers[i]);
+                }
+            }
+        }
+        if(_l %2 ==0) {
+            return bytesAverage(numbers[_l/2-1], numbers[_l/2]);
+        } else {
+            return (numbers[_l/2]);
+        }
+    }
+}
+message.txt
+6KB
